03cc128ba7482d7111c8f5338441f024796f283c
abdifahadi <md.abdifahadi@gmail.com>
2026-03-02 21:57:11 +0600

    V3.9: Brand new installer filename to bypass GitHub CDN Cache and fix ParserError

--- install.ps1
+++ install.ps1
@@ -1,1 +1,1 @@
-iwr -useb https://raw.githubusercontent.com/abdifahadi/Abdify/main/setup_abdify.ps1 | iex
+iwr -useb https://raw.githubusercontent.com/abdifahadi/Abdify/main/setup_abdify.ps1?v=d7cc209c-a4a1-4b9d-8c8a-7d96752794fb | iex
